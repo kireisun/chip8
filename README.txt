5640d02110f60f3f84717767d76e01a99301df71
kireisun <kireisun85@gmail.com>
2013-06-03 22:19:54 +0200

    opcodes
    implemented more opcodes
    fixed some errors

--- README.txt
+++ README.txt
@@ -3,6 +3,7 @@
 Chip8 interpreter
 *****************
 
-Chip8 interpreter that I started few weeks ago. It has a lot to do
+Chip8 interpreter that I started to improve my programming skills and emulation knowledge.
+It has a lot to do.
 
-Chip8 is a programming language used on some computers in the eary '80s
+Chip8 is a programming language used on some computers in the early '80s.
--- cpuEmu.cpp
+++ cpuEmu.cpp
@@ -45,8 +45,8 @@ void chip8::emulateCycle()
     opcode = memory[pc] << 8 | memory[pc + 1];
 
     // decode opcode
-    unsigned short opcode_check = opcode & 0xF000; //select for the first 4 bits, for some opcodes there is the need for more checks
-    switch(opcode_check)
+
+    switch(opcode & 0xF000) //select for the first 4 bits, for some opcodes there is the need for more checks
     {
         case 0x0000: // check instruction to execute
             switch(opcode & 0x000F)
@@ -61,14 +61,25 @@ void chip8::emulateCycle()
                     }
                     break;
                 case 0x000E: // 00EE: returns from a subroutine
-                    --sp;
-                    pc = my_stack[sp];
+                    pc = my_stack[--sp];
                     break;
             }
         case 0x1000: // 1NNN: jump to address NNN
+            pc = opcode & 0x0FFF;
+            break;
+        case 0x2000: // 2NNN: call subroutine at NNN
             my_stack[sp] = pc;
             ++sp;
             pc = opcode & 0x0FFF;
+            break;
+        case 0x3000: // 3XNN: compare Vx with NN and if they match skips the next instruction
+            if(V[opcode & 0x0F00] == opcode & 0x00FF)
+            {
+                pc = pc + 4;
+                break;
+            }
+            else
+            pc = pc + 2;
             break;
         case 0xA000: // ANNN: sets I to the address NNN
             I = opcode & 0x0FFF;
